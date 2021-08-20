2ade8381ad1e8586413d0c3b19e06ab9363b1eed
ipsingh85 <47568878+ipsingh85@users.noreply.github.com>
2021-08-20 09:52:12 +0530

    Add files via upload

---
+++ Assingment_1/codes/pre_to_post.c
@@ -0,0 +1,83 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<limits.h>
+
+
+//#include<iostream>
+//#include<limits.h>
+//using namespace std;
+//int preIndex;
+// C++ program for finding postorder
+// traversal of BST from preorder traversal
+
+ 
+// Function to find postorder traversal from
+// preorder traversal.
+void findPostOrderUtil(int pre[], int n, int minval,
+                       int maxval, int* preIndex)
+{
+ 
+    // If entire preorder array is traversed then
+    // return as no more element is left to be
+    // added to post order array.
+    if (*preIndex == n)
+        return;
+ 
+    // If array element does not lie in range specified,
+    // then it is not part of current subtree.
+    if (pre[*preIndex] < minval || pre[*preIndex] > maxval) {
+        return;
+    }
+ 
+    // Store current value, to be printed later, after
+    // printing left and right subtrees. Increment
+    // preIndex to find left and right subtrees,
+    // and pass this updated value to recursive calls.
+    int val = pre[*preIndex];
+    *preIndex=*preIndex+1;
+ 
+    // All elements with value between minval and val
+    // lie in left subtree.
+    findPostOrderUtil(pre, n, minval, val, preIndex);
+ 
+    // All elements with value between val and maxval
+    // lie in right subtree.
+    findPostOrderUtil(pre, n, val, maxval, preIndex);
+ 
+    //cout<<val<<" ";
+    printf(" %d",val);
+}
+ 
+// Function to find postorder traversal.
+void findPostOrder(int pre[], int n)
+{
+ 
+    // To store index of element to be
+    // traversed next in preorder array.
+    // This is passed by reference to
+    // utility function.
+    //int preIndex;
+    int preIndex= 0;
+ 
+    findPostOrderUtil(pre, n, INT_MIN, INT_MAX, &preIndex);
+    
+}
+ 
+// Driver code
+int main(void)
+{
+    int pre[] = { 15,10,12,11,20,18,16,19 };
+ 
+    int n = sizeof pre / sizeof pre[0];
+ 
+    // Calling function
+    findPostOrder(pre, n);
+    return 0;
+}
